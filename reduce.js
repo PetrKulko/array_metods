c444e250084a85afdf5554c80281b007d5f5f67a
Peter_Kulko <petrokulko@gmail.com>
2022-08-25 21:13:32 +0300

    reduce learning

--- reduce.js
+++ reduce.js
@@ -1,8 +1,59 @@
 const numbers = [1, 2, 3, 4, 5];
 
-const result = numbers.reduce(function(accumulator, element) {
+const sum = numbers.reduce(function(accumulator, element) {
     console.log('accumulator: ', accumulator, 'current element: ', element);
     return accumulator + element;
 }, 0);
+
+console.log(sum);
+
+const colors = ['White', 'Dark', 'Red', 'Green'];
+
+const figma = colors.reduce(function (accumulator, element) {
+    accumulator[element] = 1;
+    return accumulator;
+}, {});
+
+console.log(figma);
+
+const cards = ['White', 'Red', 'Dark', 'White'];
+
+const cardAll = cards.reduce(function (accumulator, element) {
+   if (accumulator[element]) {
+       accumulator[element] = accumulator[element] + 1;
+   } else {
+       accumulator[element] = 1;
+   }
+   return accumulator;
+}, []);
+
+console.log(cardAll);
+
+const initialValue = 0;
+const matrix = [{ x: 1 }, { x: 2 }, { x: 3 }];
+
+const total = matrix.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue.x;
+}, initialValue);
+
+console.log(total);
+
+const arrays = [[1, 2], [2, 3], [3, 4]];
+
+const total2 = arrays.reduce((accumulator, element) => {
+   return accumulator.concat(element);
+});
+
+console.log(total2);
+
+const friends = [
+    { name: 'Olya', books: ["Lord of the ring", "Harry Potter"], age: 18 },
+    { name: 'Max', books: ["Learn easy Bootstrap", "Bible"], age: 26 },
+    { name: 'Peter', books: ["Anna Karenina", "Kobzar"], age: 26 }
+];
+
+const result = friends.reduce(function(prev, curr) {
+   return [...prev, ...curr.books];
+}, []);
 
 console.log(result);
